43b39440ba865d68d879543274b5d6214c7c7d24
Mz-unitst <176293088@qq.com>
2021-12-22 08:09:57 -0800

    tuxing staring

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #define __LIBRARY__
 #include<stdio.h>
 #include<unistd.h>
-#include<ettno.h>
+#include<errno.h>
 #define vga_graph_memstart 0xA0000
 #define vga_graph_memsize 64000
 #define cursor_side 6
@@ -12,7 +12,7 @@ _syscall0(void,init_graphics)
 
 int main(void)
 {
-init_gtaphics();
+init_graphics();
 char *p=vga_graph_memstart;
 int i,j,x_pos=20,y_pos=20;
 for(i=0;i<vga_graph_memsize;i++)
